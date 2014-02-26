897856dcea571f6cba330446ba303f7d7079c26b
Mate Solymosi <mate@solymo.si>
2014-02-26 19:07:14 +0100

    Started implementing mongo storage.

--- lib/modules/mongo_storage/mongo_storage.rb
+++ lib/modules/mongo_storage/mongo_storage.rb
@@ -1,10 +1,32 @@
+require "mongo"
+
 module XAS
 	module Modules
 		module MongoStorage
 			extend self
 			
+			attr_reader :client
+			attr_reader :database
+			
 			def initialize!
+				Environment.events.on :environment, :ready do
+					@client = Mongo::MongoClient.new
+					@database = @client.db "xas"
+					
+					if Environment.modules.loaded? :core
+						
+					end
+				end
+			end
+			
+			class Persistence
+				def initialize(obj)
+					@object = obj
+				end
 				
+				def get_data
+					@object
+				end
 			end
 		end
 	end
