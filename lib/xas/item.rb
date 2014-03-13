8fed8315fad58f4b9cb9aa6485352ea4558bd10b
Mate Solymosi <mate@solymo.si>
2014-03-13 18:52:07 +0100

    Implemented item class.

--- lib/xas/item.rb
+++ lib/xas/item.rb
@@ -1,5 +1,29 @@
 module XAS
 	class Item
 		include Model
+		
+		attr_reader :id, :placeholder
+		
+		field :from, Time
+		validate :from, :presence
+		
+		field :to, Time
+		
+		def initialize(placeholder = nil, id = nil)
+			@id, @placeholder = id, placeholder
+		end
+		
+		def saved?
+			@id != nil
+		end
+		
+		def ==(other)
+			saved? ? self.id == other.id : self.equal?(other)
+		end
+		
+		protected
+			def set_id(id)
+				@id = id
+			end
 	end
 end
