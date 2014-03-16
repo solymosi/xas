b3c6b29bead0c7e53b3fa435d1ac641130985208
Mate Solymosi <mate@solymo.si>
2014-03-16 03:20:25 +0100

    Removed model testing code.

--- lib/xas/modules/core/events/register_currency.rb
+++ lib/xas/modules/core/events/register_currency.rb
@@ -8,53 +8,6 @@ module XAS::Modules::Core
 			field :name, String
 			field :code, String
 			
-			validate :name, :presence
-			validate :presence
-			validate :name do |field, value|
-				field.add_error :csibesz, :c => :d
-			end
-			
-			validate do |model|
-				model.add_error :modelerror, :a => :b
-			end
-			
-			field_array :numbers, Integer
-			field_hash :texts, String
-			validate :texts, :presence
-			validate :texts do |field, value|
-				field.add_error :fuck_you unless value == "NO!"
-			end
-			
-			validate :numbers, :each => false do |field, value|
-				field.add_error :too_few unless value.count > 1
-			end
-			
-			validate :numbers do |field, value|
-				field.add_error :less_than_ten if value < 10
-			end
-			
-			field_group :sub do
-				field :one, String
-				field :two, Integer
-				validate :two, :presence
-				
-				field_group_hash :sub2 do
-					field_group :arr do
-						field :huhu, Integer
-						
-						field_group_array :asdf do
-							field_group_array :xyz do
-								field :eee, Time
-								validate :eee, :presence
-							end
-						end
-					end
-					
-					field :test, String
-					validate :test, :presence
-				end
-			end
-			
 			def apply(cache)
 				c = cache.create references[:currency], date
 				c.name = name
