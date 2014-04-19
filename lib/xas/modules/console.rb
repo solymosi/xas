3d7b5e5de3099b7a8528355341dbe6ce1a927765
Mate Solymosi <mate@solymo.si>
2014-04-20 01:59:11 +0200

    Added console shortcuts.

--- lib/xas/modules/console.rb
+++ lib/xas/modules/console.rb
@@ -27,4 +27,24 @@ module XAS
 	def self.binding
 		super
 	end
+	
+	def self.test
+		Modules::Console::Test.run!
+	end
+	
+	def self.core
+		Modules::Core
+	end
+	
+	def self.env
+		Environment
+	end
+	
+	def self.reg
+		Environment.registry
+	end
+	
+	def self.cache
+		Environment.item_cache
+	end
 end
